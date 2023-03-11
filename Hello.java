d2c99b64c1fe12bca12fac9e4e649295659e5b60
iit27 <127539749+iit27@users.noreply.github.com>
2023-03-11 23:18:36 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/* NIM  : 13020210037
+Nama    : Iit al arwaihti
+Hari/tgl: jumat, 10 maret 2023
+waktu   : 18.30 */
+
+public class Hello {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        // TODO Auto-generated method stub
+        /* menuliskan hello ke layar */
+        System.out.print ("Hello");
+
+        /* menuliskan hello dan ganti baris */
+        System.out.print ("\nHello");
+
+        /* menuliskan hello dan ganti baris */
+        System.out.println("World");
+
+        System.out.println("Welcome");
+
+    }
+
+}
